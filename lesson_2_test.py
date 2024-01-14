e872d089e5926cf340dcb379ef671e86bb5a474b
Слава <vyacheslavdobr@gmail.com>
2024-01-14 13:17:58 +0200

    first commit

---
+++ lesson_2_test.py
@@ -0,0 +1,17 @@
+value = int(input())
+
+digit_1 = value // 1000
+digit_2 = value % 1000
+digit_3 = value % 100
+digit_4 = value % 10
+
+
+print(digit_1)
+print(digit_2 // 100)
+print(digit_3 // 10)
+print(digit_4 // 1)
+
+
+
+
+

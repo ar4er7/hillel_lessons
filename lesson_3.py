915328a57ca9a3a1c7932d1c75c7715210c1c2ee
Слава <vyacheslavdobr@gmail.com>
2024-01-16 18:24:58 +0200

    first commit

---
+++ lesson_3.py
@@ -0,0 +1,15 @@
+# value_int = -11
+#
+# if value_int > 0 and value_int <= 10:
+#     print(f"{value_int} is between 0 and 10")
+# elif value_int > 10:
+#     print(f"{value_int} is bigger than 10")
+# else:
+#     print(f"{value_int} is not less than 0")
+#
+# print("end")
+
+value_1 = 2000
+value_2 = 330
+result = "A" if value_1 > value_2 else "B"
+print(result)

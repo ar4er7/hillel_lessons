62572bb70ff3f3c9c59b20d261ff50e7536d8da2
Слава <vyacheslavdobr@gmail.com>
2024-01-17 06:11:05 +0200

    first commit

--- lesson_3.py
+++ lesson_3.py
@@ -9,7 +9,19 @@
 #
 # print("end")
 
-value_1 = 2000
-value_2 = 331
-result = "A" if value_1 > value_2 else "B"
-print(result)
+# value_1 = 2000
+# value_2 = 331
+# result = "A" if value_1 > value_2 else "B"
+# print(result)
+
+base_list = [1,2,3,4]
+print(f"base list original {base_list}")
+
+new_list = base_list * 4
+print(f"new list with original base list{new_list}")
+
+print(f"new list's 0 index {new_list[0]}")
+
+base_list[0] = 10
+print(f"updated base list {base_list}")
+print(f"new list after updating the base list {new_list}")

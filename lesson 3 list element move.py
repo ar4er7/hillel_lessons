592f09843d517f0415ba2f8c0a97ce74eaadd0e3
Слава <vyacheslavdobr@gmail.com>
2024-01-17 17:36:23 +0200

    first commit

--- lesson 3 list element move.py
+++ lesson 3 list element move.py
@@ -1,11 +1,16 @@
 original_list = ['first', 1, 5, -344, 0, 1.5, 'last']
 
+# tests
+# [12, 3, 4, 10]
+# [1]
+# []
+# [12, 3, 4, 10, 8]
+
 if len(original_list)>1:
     last_symbol = original_list[-1]
     original_list.pop(-1)
     new_list = original_list.insert(0, last_symbol)
 
 print(original_list)
-
 
 

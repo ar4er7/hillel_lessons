8ae80c74547dd8974a985733b1e9b5eb36eb29bc
Слава <vyacheslavdobr@gmail.com>
2024-01-17 16:39:21 +0200

    first commit

--- Lesson 3 home work.py
+++ Lesson 3 home work.py
@@ -1,6 +1,11 @@
-first_symbol = int(input("enter the 1st symbol:  "))
+# чтобы работать с калькулятором - на 3 запроса программы введи по очереди: перве число, оператор, второе число.
+# оператор может быть только один из 4ех: + - * /
+# если оператор / и второе число 0, то программа выдаст сообщение об ошибке.1
+# если число дробное, то разделение - точкой
+
+first_symbol = float(input("enter the 1st symbol:  "))
 operator = input("enter the operator:  ")
-second_symbol = int(input("enter the 2nd symbol:  "))
+second_symbol = float(input("enter the 2nd symbol:  "))
 result = 0
 
 if operator == "+":

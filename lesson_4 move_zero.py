193849f689034a4d3f0ed8e9385711d6fe255984
Слава <vyacheslavdobr@gmail.com>
2024-01-22 16:22:31 +0200

    first commit

--- lesson_4 move_zero.py
+++ lesson_4 move_zero.py
@@ -1,8 +1,19 @@
-number = [1, 0, 13, 0, 0, 0, 5]
-new_number = number
-index = 0
+# [0, 1, 0, 12, 3] -> [1, 12, 3, 0, 0]
+# [0] -> [0]
+# [1, 0, 13, 0, 0, 0, 5] -> [1, 13, 5, 0, 0, 0, 0]
+# [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0] -> [9, 7, 31, 45, 45, 45, 96, 0, 0, 0, 0, 0, 0, 0]
 
-while index < len(number):
-   if number[index] == 0:
-      zero = new_number.pop(index)
-      new_number.insert(zero,-1)
+number = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+digits = []
+zeros = []
+
+print(number)
+
+for index in range(len(number)):
+    if number[index] == 0:
+        zeros.append(number[index])
+    else:
+        digits.append(number[index])
+
+digits.extend(zeros)
+print(digits)

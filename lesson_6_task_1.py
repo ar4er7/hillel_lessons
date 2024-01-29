bfc516efad3fcd7d4babc0352aa4d1ab54e0d838
Слава <vyacheslavdobr@gmail.com>
2024-01-29 06:13:18 +0200

    first commit

--- lesson_6_task_1.py
+++ lesson_6_task_1.py
@@ -1,5 +1,6 @@
 children = []
 long_names = []
+ages_sum = 0
 
 dudes = [{"name": "John", "age": 41},
          {"name": "Batman", "age": 15},
@@ -8,24 +9,27 @@ dudes = [{"name": "John", "age": 41},
          {"name": "Timmy", "age": 99},
          {"name": "Volodymyr", "age": 41}
          ]
-min_age = dudes[0].get("age")
-max_name_length = len(dudes[0].get("name"))
+min_age = dudes[0].get("age") #возраст первого человека указываем как минимальный
+max_name_length = len(dudes[0].get("name")) #длину первого имени указываем, как максимальную
 
-for man in dudes:
+for man in dudes: #находим самый маленький возраст и кладем его в min_age
     if man.get("age") < min_age:
         min_age = man.get("age")
 
-    if len(man.get("name")) > max_name_length:
-        max_name_length = len(man.get("name"))
-    print(max_name_length)
+    ages_sum += man.get("age") #суммируем все возрасты в переменную ages_sum
 
-for man in dudes:
+    if len(man.get("name")) > max_name_length: #находим длину самого длинного имени икладем его в max_name_lenth
+        max_name_length = len(man.get("name"))
+
+average_age = ages_sum / len(dudes)
+
+for man in dudes: #все элементы массива с минимальным возрастом кладем в массив children
     if man.get("age") == min_age:
         children.append(man.get("name"))
 
-    if man.get("name") == max_name_length:
+    if len(man.get("name")) == max_name_length:
         long_names.append(man.get("name"))
 
-print(children)
-print(long_names)
-
+print("youngest people are: ", children)
+print("longest names: ", long_names)
+print("average age: ", int(average_age))

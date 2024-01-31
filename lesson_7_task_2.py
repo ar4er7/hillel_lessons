d6a99a3e36b9c8591e174eb974857cf0ab98683d
Слава <vyacheslavdobr@gmail.com>
2024-01-31 04:15:03 +0200

    first commit

---
+++ lesson_7_task_2.py
@@ -0,0 +1,20 @@
+def correct_sentence(text: str):
+    result = ''
+    if text[0].islower():
+        result += text[0].capitalize() + text[1:]
+    else:
+        result = text
+
+    if text[-1] != '.':
+        result += '.'
+    return result
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
+
+# print(correct_sentence("Greetings. Friends"))

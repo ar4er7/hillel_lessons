2ef3d58c586d35b1bb6ed2af5a22915dcbc285c7
Viacheslav Dobrovolskiy <48251417+ar4er7@users.noreply.github.com>
2024-02-14 16:56:03 +0200

    Update

---
+++ lesson_11_2.py
@@ -0,0 +1,12 @@
+def is_even(number):
+    num = str(number)
+    
+    if int(num[-1]) in [1, 3, 5, 7, 9]:
+        return False
+    return True   
+#print (is_even(**2)
+
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
+print('Ok'

1136a791bd8a8cb8d469cc1d9a6b27d25a94019b
Слава <vyacheslavdobr@gmail.com>
2024-02-14 17:07:17 +0200

    first commit

--- lesson_11_2.py
+++ lesson_11_2.py
@@ -9,4 +9,4 @@ def is_even(number):
 assert is_even(2494563894038**2) == True, 'Test1'
 assert is_even(1056897**2) == False, 'Test2'
 assert is_even(24945638940387**3) == False, 'Test3'
-print('Ok'
+print('Ok')

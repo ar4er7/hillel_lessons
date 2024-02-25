a39d614c9decb4e055e72e7428e8e660c0315a0c
Слава <vyacheslavdobr@gmail.com>
2024-02-25 16:35:14 +0200

    создал ошибку для 11го студента (верхнюю планку можно менять) переопределил метод __eq__

--- lesson_13_task_1.py
+++ lesson_13_task_1.py
@@ -1,5 +1,4 @@
 class Human:
-
     def __init__(self, gender, age, first_name, last_name):
         self.gender = gender
         self.age = age
@@ -11,10 +10,17 @@ class Human:
 
 
 class Student(Human):
-
     def __init__(self, gender, age, first_name, last_name, record_book):
         super().__init__(gender, age, first_name, last_name)
         self.record_book = record_book
+
+    def __eq__(self, other):
+        if isinstance(other, Student):
+            return self.__dict__ == other.__dict__
+        return NotImplemented
+
+    def __hash__(self):
+        return hash(str(self))
 
     def __str__(self):
         return f'Student: {super().__str__()}, record book: {self.record_book}'
@@ -25,8 +31,8 @@ class Group:
     def __init__(self, number):
         self.number = number
         self.group = set()
-        self.count = 0
-        self.__capacity = 10
+        self.count = 0 #текущее кол-во студентов в группе
+        self.__capacity = 10 #максимальная вместимость группы
 
     def add_student(self, student):
         if self.count < self.get_capacity():
@@ -88,7 +94,7 @@ st7 = Student('Female', 25, 'Ignat', 'Bochkin', 'AN149')
 st8 = Student('Female', 25, 'Egor', 'Kopytin', 'AN112')
 st9 = Student('Female', 25, 'Lev', 'Sgrivoy', 'AN134')
 st10 = Student('Female', 25, 'Galina', 'Blanka', 'AN167')
-st11 = Student('Female', 25, 'Nikita', 'Perviu', 'AN190')
+st11 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
 gr = Group('PD1')
 gr.add_student(st1)
 gr.add_student(st2)
@@ -97,15 +103,16 @@ print(gr)
 print(str(gr.find_student('Jobs')))
 
 assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs') == st1
 assert gr.find_student('Jobs2') is None, 'Test2'
 assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод пошуку має повертати екземпляр'
 
 gr.delete_student('Taylor')
-print(gr)  # Only one student
-#
+# print(gr)  # Only one student
+
 gr.delete_student('Taylor')  # No error!
 
-"""тут начинаю добавлять 10 студентов для 14го занятия"""
+"""тут начинаю добавлять 10 студентов для 1 задачи 14го занятия"""
 
 gr.add_student(st2)
 gr.add_student(st3)
@@ -116,13 +123,14 @@ gr.add_student(st7)
 gr.add_student(st8)
 gr.add_student(st9)
 gr.add_student(st10)
-print(gr.count)
+print(gr.count) #на этом моменте группа заполнена максимально. Следующий студернт - вызовет ошибку
 
 try:
     gr.add_student(st11)
 except OverCountException as err:
     print(err.get_exception_message())
     print(f"should be {gr.get_capacity()}, or less")
+
 
 
 

9168bde26c6cac67ceed2e3dda412468adeea962
Слава <vyacheslavdobr@gmail.com>
2024-02-25 16:44:10 +0200

    перенес main в папку с классами

--- lesson_14/main.py
+++ lesson_14/main.py
@@ -1,5 +1,5 @@
-from lesson_14.Student import Student
-from lesson_14.Group import Group
+from Student import Student
+from Group import Group
 
 st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
 st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
